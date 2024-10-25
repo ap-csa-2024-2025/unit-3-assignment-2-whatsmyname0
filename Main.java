2570cac593a624c9522a61ffba13c96035fc7d36
Whatsmyname0 <mikepham2007@gmail.com>
2024-10-25 10:15:28 -0700

    yes

--- Main.java
+++ Main.java
@@ -4,6 +4,18 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    int rand1 = (int) (Math.random() * 51);
+    int rand2 = (int) (Math.random() * 51) + 50;
+    System.out.println(rand1 + " + " + rand2 + " = ?");
+    Scanner sc = new Scanner(System.in);
+    int ans1 = sc.nextInt();
+    if (ans1 == (rand1 + rand2))
+    {
+      System.out.println("Correct");
+    }
+    else
+    {
+      System.out.println("Wrong");
+    }
   }
 }
